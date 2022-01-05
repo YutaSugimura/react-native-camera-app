3237b897ed120ef9179aec0ecfd3ec589035036b
YutaSugimura <nextc3m@gmail.com>
2022-01-05 13:30:45 +0900

    Implementing the camera App

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,59 @@
  * @format
  */
 
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useEffect, useRef, useState} from 'react';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {Camera, useCameraDevices} from 'react-native-vision-camera';
 
 const App: React.VFC = () => {
+  const devices = useCameraDevices();
+  const device = devices.back;
+
+  const cameraRef = useRef<Camera>(null);
+
+  const [authorized, setAuthorized] = useState<boolean>(false);
+
+  useEffect(() => {
+    (async () => {
+      const cameraPermission = await Camera.getCameraPermissionStatus();
+
+      if (cameraPermission === 'authorized') {
+        setAuthorized(true);
+      } else if (
+        cameraPermission === 'not-determined' ||
+        cameraPermission === 'denied'
+      ) {
+        const newCameraPermission = await Camera.requestCameraPermission();
+        if (newCameraPermission === 'authorized') {
+          setAuthorized(true);
+        }
+      }
+    })();
+  }, []);
+
+  if (device === undefined || !authorized) {
+    return (
+      <SafeAreaView>
+        <Text>loading...</Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
+    <Camera
+      style={styles.container}
+      ref={cameraRef}
+      device={device}
+      isActive={authorized}
+      photo={true}
+    />
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
